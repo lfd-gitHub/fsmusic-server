a00c839e6841ab77b448b6d7c658cd42324b979e
lfd <lfd_wy@163.com>
2021-12-19 19:22:59 +0800

    add cross origin

--- src/main/java/com/lfd/fsmusic/controller/DefaultController.java
+++ src/main/java/com/lfd/fsmusic/controller/DefaultController.java
@@ -1,10 +1,12 @@
 package com.lfd.fsmusic.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/hello")
 public class DefaultController {
     
