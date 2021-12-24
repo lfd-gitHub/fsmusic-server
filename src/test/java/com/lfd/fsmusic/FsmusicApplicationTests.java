a4037ebdf0f5b79dde72829b6d3f2385e4e0332e
lfd <lfd_wy@163.com>
2021-12-24 20:13:12 +0800

    solved (Caused by: java.net.UnknownHostException: `${MYSQL_ADDRESS}`: Temporary failure in name reso...) actually caused by ((Tests run: 1, Failures: 0, Errors: 1, Skipped: 0 )  )

---
+++ src/test/java/com/lfd/fsmusic/FsmusicApplicationTests.java
@@ -0,0 +1,14 @@
+package com.lfd.fsmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsmusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("---");
+	}
+
+}
