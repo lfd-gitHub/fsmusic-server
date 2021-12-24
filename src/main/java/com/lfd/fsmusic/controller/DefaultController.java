fdb842b552063963e833a3fcccc7a1f90e07f33f
lfd <lfd_wy@163.com>
2021-12-24 19:11:05 +0800

    change diff files

--- src/main/java/com/lfd/fsmusic/controller/DefaultController.java
+++ src/main/java/com/lfd/fsmusic/controller/DefaultController.java
@@ -1,8 +1,11 @@
 package com.lfd.fsmusic.controller;
 
+import java.time.LocalDateTime;
+
 import com.lfd.fsmusic.repository.RoleRepository;
 import com.lfd.fsmusic.repository.UserRepository;
 import com.lfd.fsmusic.repository.entity.Role;
+import com.lfd.fsmusic.repository.entity.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -12,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/hello")
+@RequestMapping("/install")
 public class DefaultController {
     
     @Autowired
@@ -22,16 +25,14 @@ public class DefaultController {
 
     @GetMapping
     public String install(){
-        // User user = new User();
-        // user.setUsername("username"+LocalDateTime.now()
-        // .atZone(ZoneOffset.systemDefault())
-        // .toInstant().toEpochMilli());
-        // user.setPassword("password");
-        // user.setNickname("nickname");
-        // user.setEnabled(true);
-        // user.setLocked(false);
-        // user.setLastLoginIp("127.0.0.1");
-        // user.setLastLoginTime(LocalDateTime.now());
+        User user = new User();
+        user.setUsername("admin");
+        user.setPassword("1234567");
+        user.setNickname("admin");
+        user.setEnabled(true);
+        user.setLocked(false);
+        user.setLastLoginIp("127.0.0.1");
+        user.setLastLoginTime(LocalDateTime.now());
 
     
         Role roleAdmin = new Role();
@@ -42,9 +43,9 @@ public class DefaultController {
         roleUser.setName("User");
         roleUser.setTitle("管理员");
         
-        // user.getRoles().add(role);
+        user.getRoles().add(roleAdmin);
+        user.getRoles().add(roleUser);
 
-        // userRepository.save(user);
         Role savedAdmin = roleRepository.findByName("Admin");
         System.out.println("admin-role:" + savedAdmin);
         if(savedAdmin == null) roleRepository.save(roleAdmin);
@@ -52,7 +53,9 @@ public class DefaultController {
         System.out.println("user-role:" + savedUser);
         if(savedUser == null) roleRepository.save(roleUser);
 
-        //user.setRoles(Arrays.asList(new Role[]{role}));
+        User saveUser = userRepository.getByUsername("admin");
+        if(saveUser == null) userRepository.save(user);
+
         return "success:" + userRepository.findAll();
     }
 }
